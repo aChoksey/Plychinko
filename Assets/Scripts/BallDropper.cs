87583a67343e4191950d5a3862dc42689ae55d0c
aChoksey <aidanleefx@gmail.com>
2026-09-12 21:55:13 -0400

    - Added random ball dropping on  left mouse click

---
+++ Assets/Scripts/BallDropper.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallDropper : MonoBehaviour
+{
+    public GameObject BallPrefab; // capitalize public variables
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            DropBall();
+        }
+    }
+
+    private void DropBall()
+    {
+        // pick a starting position
+        Vector3 spawnPosition = GetSpawnPosition();
+        
+        // create ball at that position
+        Instantiate(BallPrefab, spawnPosition,
+            Quaternion.identity); //what to create, where to create it, how to rotate it ?
+
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        Vector3 leftEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        Vector3 rightEdge = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, Camera.main.nearClipPlane));
+        
+        float randomX = Random.Range(leftEdge.x + 1, rightEdge.x - 1);
+        
+        Vector3 spawnPosition = new Vector3(randomX ,4.5f, 0f);
+        
+        return spawnPosition;
+    }
+}
